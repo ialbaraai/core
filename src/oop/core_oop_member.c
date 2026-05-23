0cb9c8371c5c289f948674dda6537d5d19627788
ialbaraai <colnarxy@gmail.com>
2026-05-23 07:56:50 +0400

    Reafctor: fixed `core_member_init` & `core_member_init` functions.

--- src/oop/core_oop_member.c
+++ src/oop/core_oop_member.c
@@ -5,8 +5,6 @@
 
 member_t core_member_init(const char* name, const void* holder, const size_t value_size, const visibility_t visibility, copy_function_t copy_function, destroy_function_t destroy_function)
 {
-    if (!name) return;
-
     member_t member = {._Member_Name = core_string_init_data(strlen(name) + 1, name), ._Member_Holder = holder, ._Member_Value_Size = value_size,._Member_Visibility = visibility,._Member_Value = malloc(value_size)};
 
     if (!member._Member_Value)
@@ -21,8 +19,6 @@ member_t core_member_init(const char* name, const void* holder, const size_t val
 }
 member_t core_member_init_data(const char* name, const void* holder, const void* value, const size_t value_size, const visibility_t visibility, copy_function_t copy_function, destroy_function_t destroy_function)
 {
-    if (!name) return;
-    
     member_t member = core_member_init(name, holder, value_size, visibility, copy_function, destroy_function);
 
     if (member._Member_Value)
