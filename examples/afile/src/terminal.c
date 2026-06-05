f35344b0210d3e4118b49fd0e61e50b78cce7ddd
ialbaraai <colnarxy@gmail.com>
2026-06-05 16:55:14 +0400

    refactor: `win32` support for `afile`

--- examples/afile/src/terminal.c
+++ examples/afile/src/terminal.c
@@ -1,5 +1,28 @@
 #include "terminal.h"
 
+#ifdef _WIN32
+#include <windows.h>
+
+static DWORD old_mode;
+static HANDLE stdin_handle;
+
+void terminal_raw_mode_enter(void)
+{
+    stdin_handle = GetStdHandle(STD_INPUT_HANDLE);
+
+    GetConsoleMode(stdin_handle, &old_mode);
+
+    DWORD raw_mode = old_mode;
+    raw_mode &= ~(ENABLE_ECHO_INPUT | ENABLE_LINE_INPUT);
+
+    SetConsoleMode(stdin_handle, raw_mode);
+}
+
+void terminal_raw_mode_exit(void)
+{
+    SetConsoleMode(stdin_handle, old_mode);
+}
+#else
 #include <termios.h>
 #include <unistd.h>
 
@@ -17,3 +40,4 @@ void terminal_raw_mode_exit()
 {
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &_old);
 }
+#endif
